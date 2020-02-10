9930c94cb222ef6e91933cd5c8d40d2c7a7fe891
nowyTR <nowy.inheritance@gmail.com>
2020-02-10 12:22:35 +0100

    add authentication

--- webapp/gatsby-ssr.js
+++ webapp/gatsby-ssr.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react'
+import { navigate } from 'gatsby'
+
+import { AuthProvider } from 'react-use-auth'
+
+export const wrapRootElement = ({ element }) => (
+  <AuthProvider
+    navigate={navigate}
+    auth0_domain={process.env.AUTH0_DOMAIN}
+    auth0_client_id={process.env.AUTH0_CLIENT_ID}
+  >
+    {element}
+  </AuthProvider>
+)
--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -1,21 +1,24 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
+import { Flex, Heading, Box, Button } from 'rebass'
+import { useAuth } from 'react-use-auth'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { login, user, isAuthenticated } = useAuth()
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Heading fontSize={[5, 6, 7]}>Hello</Heading>
+      <p>Write a landing page for anything</p>
+      {isAuthenticated() ? <p>Hello {user.nickname}</p> : null}
+      <Button bg="primary" onClick={login}>
+        Get started
+      </Button>
+    </Layout>
+  )
+}
 
 export default IndexPage
