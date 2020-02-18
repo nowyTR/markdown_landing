b48d9ed05fc33e305d83ef48a2d992561c16e95c
nowyTR <nowy.inheritance@gmail.com>
2020-02-18 16:28:39 +0100

    create/update user

--- server/src/mutations.ts
+++ server/src/mutations.ts
@@ -1,4 +1,4 @@
-import { getItem } from 'simple-dynamodb'
+import { getItem, updateItem } from 'simple-dynamodb'
 
 type UserParams = {
   userId: string
@@ -6,17 +6,50 @@ type UserParams = {
 
 async function updateUser(parent, args: UserParams): Promise<User> {
   const { userId } = args
-  const user = await getItem({
+  const result = await getItem({
     TableName: process.env.USER_TABLE!,
     Key: {
       userId
     }
   })
 
+  let user = result.Item
+
+  if (user != null) {
+    const result = await updateItem({
+      TableName: process.env.USER_TABLE!,
+      Key: {
+        userId
+      },
+      UpdateExpression: 'SET lastSignedInAt = :lastSignedInAt',
+      ExpressionAttributeValues: {
+        ':lastSignedInAt': new Date().toISOString()
+      },
+      ReturnValues: 'ALL_NEW'
+    })
+
+    user = result.Attributes
+  } else {
+    const result = await updateItem({
+      TableName: process.env.USER_TABLE!,
+      Key: {
+        userId
+      },
+      UpdateExpression: 'SET createdAt = :createdAt, lastSignedInAt = :lastSignedInAt',
+      ExpressionAttributeValues: {
+        ':createdAt': new Date().toISOString(),
+        ':lastSignedInAt': new Date().toISOString()
+      },
+      ReturnValues: 'ALL_NEW'
+    })
+
+    user = result.Attributes
+  }
+
   return {
     userId,
-    createdAt: user.Item ? user.Item.createdAt : null,
-    lastSignedInAt: user.Item ? user.Item.lastSignedInAt : null
+    createdAt: user ? user.createdAt : null,
+    lastSignedInAt: user ? user.lastSignedInAt : null
   }
 }
 
